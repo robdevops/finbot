0b4ad6238c3b1e7c12d7e074346ab131a21f24c8
rob <rstevens>
2022-08-23 14:49:42 +1000

    Add earnings reminders and remove yfinance dependency

--- lambda_function.py
+++ lambda_function.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
 
 import os, time, urllib.parse, json
-from datetime import datetime
+import datetime
 from dotenv import load_dotenv
 import requests
-import yfinance as yf
 
 def lambda_handler(event,context):
     load_dotenv()
@@ -29,13 +28,22 @@ def lambda_handler(event,context):
     if os.getenv('price_updates'):
         config_price_updates = os.getenv("price_updates", 'False').lower() in ('true', '1', 't')
 
+    if os.getenv('earnings_reminder'):
+        config_earnings_reminder = os.getenv("earnings_reminder", 'False').lower() in ('true', '1', 't')
+
+    config_earnings_reminder_days = 3 # default
+    if os.getenv('earnings_reminder_days'):
+        config_earnings_reminder_days = os.getenv('earnings_reminder_days') 
+        config_earnings_reminder_days = int(config_earnings_reminder_days)
+
     config_price_updates_percentage = 10 # default
     if os.getenv('price_updates_percentage'):
         config_price_updates_percentage = os.getenv('price_updates_percentage') 
         config_price_updates_percentage = float(config_price_updates_percentage)
 
-    date = datetime.today().strftime('%Y-%m-%d')
-    #date = '2022-08-13'
+    #time_now = datetime.datetime.today() # 2022-08-23 01:35:20.310961
+    time_now = datetime.datetime.utcnow() # 2022-08-22 15:35:20.311000
+    date = str(time_now).split(' ')[0] # 2022-08-22
     
     class BearerAuth(requests.auth.AuthBase):
         def __init__(self, token):
@@ -97,58 +105,16 @@ def lambda_handler(event,context):
         return data['holdings']
 
     def transform_tickers(holdings):
-        tickers = {}
+        tickers = []
         for holding in holdings:
             symbol = holding['symbol']
-            name = holding['name']
             market = holding['market']
-
-            # shorten long names to reduce line wrap on mobile
-            name = name.replace("- Ordinary Shares - Class A", "")
-            name = name.replace("Global X Funds - Global X ", "")
-            name = name.replace("- New York Shares", "")
-            name = name.replace("Co (The)", " ")
-            name = name.replace("- ADR", "")
-            name = name.replace(" Index", " ")
-            name = name.replace(" Enterprises", " ")
-            name = name.replace(" Enterprise", " ")
-            name = name.replace(" Enterpr", " ")
-            name = name.replace(" Group", " ")
-            name = name.replace(" Co.", " ")
-            name = name.replace(" Holdings", " ")
-            name = name.replace(" Infrastructure", " Infra")
-            name = name.replace(" Technologies", " ")
-            name = name.replace(" Technology", " ")
-            name = name.replace(" Plc", " ")
-            name = name.replace(" Limited", " ")
-            name = name.replace(" Ltd", " ")
-            name = name.replace(" Incorporated", " ")
-            name = name.replace(" Inc", " ")
-            name = name.replace(" Corporation", " ")
-            name = name.replace(" Corp", " ")
-            name = name.replace(" Australian", " Aus")
-            name = name.replace(" Australia", " Aus")
-            name = name.replace("Etf", "ETF")
-            name = name.replace("Tpg", "TPG")
-            name = name.replace("Rea", "REA")
-            name = name.replace(" .", " ")
-            name = name.replace("  ", " ")
-            name = name.rstrip()
-            if symbol == 'QCLN':
-                name = 'NASDAQ Clean Energy ETF'
-            if symbol == 'LIS' and market == 'ASX':
-                name = 'LI-S Energy'
-            if symbol == 'TSM':
-                name = 'Taiwan Semiconductor'
-            if symbol == 'MAP' and market == 'ASX':
-                name = 'Microba Life Sciences'
-            if symbol == 'DRNA' and market == 'NASDAQ':
-                continue
-
             if market == 'ASX':
-                tickers[symbol + '.AX'] = name
+                tickers.append(symbol + '.AX')
             elif market == 'NASDAQ' or market == 'NYSE' or market == 'BATS':
-                tickers[symbol] = name
+                if symbol == 'DRNA':
+                    continue
+                tickers.append(symbol)
             else:
                 continue
         return tickers
@@ -156,6 +122,12 @@ def lambda_handler(event,context):
     def prepare_trade_payload(service, trades):
         print(f"Preparing payload: {service}")
         trade_payload = []
+        if service == 'telegram':
+            trade_payload.append(f"<b>Today's trades:</b>")
+        elif service == 'slack':
+            trade_payload.append(f"*Today's trades:*")
+        else:
+            trade_payload.append(f"**Today's trades:**")
         for trade in trades:
             trade_id = trade['id']
             portfolio = trade['portfolio']
@@ -202,7 +174,7 @@ def lambda_handler(event,context):
         return trade_payload
     
     def webhook_write(url, payload):
-        # slack: "unfurl_links": false, "unfurl_media": false
+        # slack todo: "unfurl_links": false, "unfurl_media": false
         try:
             r = requests.post(url, headers={'Content-type': 'application/json'}, json={"text":payload})
         except:
@@ -227,42 +199,30 @@ def lambda_handler(event,context):
     def chunker(seq, size):
         return (seq[pos:pos + size] for pos in range(0, len(seq), size))
 
-    def percentage_change(previous, current):
-        if current == previous:
-            return 0
-        try:
-            return (abs(current - previous) / previous) * 100.0
-        except ZeroDivisionError:
-            return float('inf')
-    
-    def yahoo_get_prices(tickers):
-        alert = []
-        alert_telegram = []
-        all_tickers_string = ' '.join(tickers)
-        print(f"Fetching Yahoo price histories for {all_tickers_string}")
-        price_data = yf.download(all_tickers_string, period="2d", group_by='ticker')
-        return price_data
-
-    def prepare_price_payload(service, price_data, config_price_updates_percentage):
+    def prepare_price_payload(service, yahoo_output, config_price_updates_percentage):
         price_payload = []
-        for ticker in tickers:
-            previous = price_data[ticker].to_numpy()[0][4]
-            current = price_data[ticker].to_numpy()[1][4]
-            percentage = percentage_change(previous, current)
-            percentage = round(percentage, 2)
-            if percentage > config_price_updates_percentage:
+        if service == 'telegram':
+            price_payload.append(f"<b>Price alerts:</b>")
+        elif service == 'slack':
+            price_payload.append(f"*Price alerts:*")
+        else:
+            price_payload.append(f"**Price alerts:**")
+        for ticker in yahoo_output:
+            percentage = yahoo_output[ticker]['percent_change']
+            title = yahoo_output[ticker]['title']
+            if abs(percentage) > config_price_updates_percentage:
                 yahoo_url = 'https://finance.yahoo.com/quote/' + ticker
-                if previous > current:
+                if percentage < 0:
                     if service == 'telegram':
-                        price_payload.append('🔻' + tickers[ticker] + ' (<a href="' + yahoo_url + '">' + ticker + '</a>) ' + "day change: -" + str(percentage) + '%')
+                        price_payload.append(f"🔻 {title} (<a href='{yahoo_url}'>{ticker}</a>) day change: {str(percentage)}%")
                     else:
-                        price_payload.append(f'🔻{tickers[ticker]} (<{yahoo_url}|{ticker}>) day change: -{str(percentage)}%')
+                        price_payload.append(f"🔻 {title} (<{yahoo_url}|{ticker}>) day change: {str(percentage)}%")
                 else:
                     if service == 'telegram':
-                        price_payload.telegram.append('⬆️ ' + tickers[ticker] + ' (<a href="' + yahoo_url + '">' + ticker + '</a>) ' + "day change: " + str(percentage) + '%')
+                        price_payload.append(f"⬆️  {title} (<a href='{yahoo_url}'>{ticker}</a>) day change: {str(percentage)}%")
                     else:
-                        price_payload.append(f'⬆️ {tickers[ticker]} (<{yahoo_url}|{ticker}>) day change: {str(percentage)}%')
-        print(len(price_payload), f"holdings moved more than {config_price_updates_percentage}%")
+                        price_payload.append(f"⬆️  {title} (<{yahoo_url}|{ticker}>) day change: {str(percentage)}%")
+        print(len(price_payload)-1, f"holdings moved more than {config_price_updates_percentage}%")
         return price_payload
 
     def payload_wrapper(service, url, chunks):
@@ -277,15 +237,126 @@ def lambda_handler(event,context):
             if count < len(list(chunks)):
                 time.sleep(1) # workaround potential API throttling
 
+    def to_localtime(utc_datetime):
+        now_timestamp = time.time()
+        offset = datetime.datetime.fromtimestamp(now_timestamp) - datetime.datetime.utcfromtimestamp(now_timestamp)
+        return utc_datetime + offset
+
+    def prepare_earnings_reminder_payload(service, yahoo_output):
+        earnings_reminder_payload = []
+        if service == 'telegram':
+            earnings_reminder_payload.append(f"<b>Upcoming earnings:</b>")
+        elif service == 'slack':
+            earnings_reminder_payload.append(f"*Upcoming earnings:*")
+        else:
+            earnings_reminder_payload.append(f"**Upcoming earnings:**")
+        for ticker in yahoo_output:
+            title = yahoo_output[ticker]['title']
+            yahoo_url = 'https://finance.yahoo.com/quote/' + ticker
+            try:
+                earnings_date = yahoo_output[ticker]['earnings_date']
+            except KeyError:
+                continue
+            #earnings_date = earnings_reminders[ticker]
+            #ticker_description = tickers[ticker]
+            if earnings_date:
+                if service == 'telegram':
+                    earnings_reminder_payload.append(f"📣 {title} (<a href='{yahoo_url}'>{ticker}</a>) reports on {earnings_date}")
+                else:
+                    earnings_reminder_payload.append(f"📣 {title} (<{yahoo_url}|{ticker}>) reports on {earnings_date}")
+        return earnings_reminder_payload
+
+    def yahoo_fetch(tickers):
+        print(f"fetching {len(tickers)} holdings from Yahoo")
+        yahoo_output = {}
+        now = int(time.time())
+        soon = now + config_earnings_reminder_days * 86400
+        url = 'https://query1.finance.yahoo.com/v7/finance/quote?symbols=' + ','.join(tickers)
+        url2 = 'https://query2.finance.yahoo.com/v7/finance/quote?symbols=' + ','.join(tickers)
+        try:
+            r = requests.get(url, headers={'Content-type': 'application/json', 'User-Agent': 'Mozilla/5.0'})
+        except:
+            print(f"Failed to query Yahoo. Trying alternate URL.")
+            try:
+                r = requests.get(url2, headers={'Content-type': 'application/json', 'User-Agent': 'Mozilla/5.0'})
+            except:
+                print(f"Failed to query alternate URL. Giving up.")
+                return []
+            if r.status_code != 200:
+                print(f"alternate URL returned {r.status_code}. Giving up.")
+                return []
+        if r.status_code != 200:
+            print(f"Yahoo returned {r.status_code}. Trying alternate URL.")
+            try:
+                r = requests.get(url2, headers={'Content-type': 'application/json', 'User-Agent': 'Mozilla/5.0'})
+            except:
+                print(f"Failed to query alternate URL. Giving up.")
+                return []
+            if r.status_code != 200:
+                print(f"alternate URL returned {r.status_code}. Giving up.")
+                return []
+        data = r.json()
+        data = data['quoteResponse']
+        data = data['result']
+        for item in data:
+            #print(json.dumps(ticker, indent=4, sort_keys=True))
+            ticker = item['symbol']
+            title = item['longName']
+            percent_change = item['regularMarketChangePercent']
+            percent_change = round(percent_change, 2)
+
+            # shorten long names to reduce line wrap on mobile
+            title = title.replace("First Trust NASDAQ Clean Edge Green Energy Index Fund", "NASDAQ Clean Energy ETF")
+            title = title.replace("Global X ", "")
+            title = title.replace("The ", "")
+            title = title.replace(" Australian", " Aus")
+            title = title.replace(" Australia", " Aus")
+            title = title.replace(" Infrastructure", " Infra")
+            title = title.replace(" Manufacturing Company", " ")
+            title = title.replace(" Limited", " ")
+            title = title.replace(" Ltd", " ")
+            title = title.replace(" Holdings", " ")
+            title = title.replace(" Corporation", " ")
+            title = title.replace(" Incorporated", " ")
+            title = title.replace(" incorporated", " ")
+            title = title.replace(" Technologies", " ")
+            title = title.replace(" Technology", " ")
+            title = title.replace(" Enterprises", " ")
+            title = title.replace(" Ventures", " ")
+            title = title.replace(" Co.", " ")
+            title = title.replace(" Tech ", " ")
+            title = title.replace(" Company", " ")
+            title = title.replace(" Tech ", " ")
+            title = title.replace(" Group", " ")
+            title = title.replace(", Inc", " ")
+            title = title.replace(" Inc", " ")
+            title = title.replace(" Plc", " ")
+            title = title.replace(" plc", " ")
+            title = title.replace(" Index", " ")
+            title = title.replace(" .", " ")
+            title = title.replace(" ,", " ")
+            title = title.replace("  ", " ")
+            try:
+                earningsTimestamp = item['earningsTimestamp']
+                earningsTimestampStart = item['earningsTimestampStart']
+                earningsTimestampEnd = item['earningsTimestampEnd']
+            except (KeyError, IndexError):
+                pass
+            if earningsTimestamp == earningsTimestampStart == earningsTimestampEnd: # if not estimate
+                if earningsTimestamp > now and earningsTimestamp < soon:
+                    human_timestamp = datetime.datetime.fromtimestamp(earningsTimestamp)
+                    yahoo_output[ticker] = { "title": title, "percent_change": percent_change, "earnings_date": str(human_timestamp) }
+            else:
+                yahoo_output[ticker] = { "title": title, "percent_change": percent_change}
+        return yahoo_output
+
 # MAIN #
     token = sharesight_get_token(sharesight_auth_data)
     portfolios = sharesight_get_portfolios()
-    trades = []
-    holdings = []
-    tickers = {}
 
     # get trades from sharesight
     if config_trade_updates:
+        trades = []
         for portfolio_name in portfolios:
             portfolio_id = portfolios[portfolio_name]
             trades = trades + sharesight_get_trades(portfolio_name, portfolio_id)
@@ -294,32 +365,46 @@ def lambda_handler(event,context):
         else:
             print(f"No trades found for {date}")
 
-    # get prices from yahoo
-    if config_price_updates:
+    # get holdings from sharesight
+    if config_price_updates or config_earnings_reminder:
+        holdings = []
+        tickers = []
         for portfolio_name in portfolios:
             portfolio_id = portfolios[portfolio_name]
             holdings = holdings + sharesight_get_holdings(portfolio_name, portfolio_id)
-        tickers = transform_tickers(holdings)
-        price_data = yahoo_get_prices(tickers)
-        #print(json.dumps(tickers, indent=4, sort_keys=True))
+            tickers = transform_tickers(holdings)
+
+    # get yahoo data
+    if config_price_updates or config_earnings_reminder:
+        yahoo_output = yahoo_fetch(tickers)
 
     # prep and send payloads
     for service in webhooks:
         url = webhooks[service]
-        if trades:
-            print(f"Preparing {service} payload")
-            trade_payload = prepare_trade_payload(service, trades)
-            print(f"Sending to {service}")
-            chunks = chunker(price_payload, 20)
-            payload_wrapper(service, url, chunks)
-        if tickers:
-            print(f"preparing {service} payload")
-            price_payload = prepare_price_payload(service, price_data, config_price_updates_percentage)
-            if price_payload:
-                price_payload_string = '\n'.join(price_payload)
-                print(f"{price_payload_string}")
+        if config_trade_updates:
+            if trades:
+                print(f"Preparing trade payload for {service}")
+                trade_payload = prepare_trade_payload(service, trades)
+                print(f"Sending to {service}")
                 chunks = chunker(price_payload, 20)
                 payload_wrapper(service, url, chunks)
-            else:
-                print(f"{service}: no holdings changed by {config_price_updates_percentage}% in the last session.")
+        if config_price_updates:
+            if tickers:
+                print(f"preparing price change payload for {service}")
+                price_payload = prepare_price_payload(service, yahoo_output, config_price_updates_percentage)
+                if price_payload:
+                    price_payload_string = '\n'.join(price_payload)
+                    print(f"{price_payload_string}")
+                    chunks = chunker(price_payload, 20)
+                    payload_wrapper(service, url, chunks)
+                else:
+                    print(f"{service}: no holdings changed by {config_price_updates_percentage}% in the last session.")
+        if config_earnings_reminder:
+            print(f"preparing earnings reminder payload for {service}")
+            earnings_reminder_payload = prepare_earnings_reminder_payload(service, yahoo_output)
+            earnings_reminder_payload_string = '\n'.join(earnings_reminder_payload)
+            print(f"Sending earnings reminders to {service}")
+            print(earnings_reminder_payload_string)
+            chunks = chunker(earnings_reminder_payload, 20)
+            payload_wrapper(service, url, chunks)
 
