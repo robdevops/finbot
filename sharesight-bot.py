3808add9b6bb854010c68f3a119cf1efe7515818
rob <rstevens>
2022-08-23 19:35:07 +1000

    credits

--- sharesight-bot.py
+++ sharesight-bot.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 
+# thank you # https://www.reddit.com/r/sheets/wiki/apis/finance/#wiki_yahoo_finance_api
+
 from lambda_function import lambda_handler
 
 lambda_handler("my", "test")
