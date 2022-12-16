a958e4ef5262f795829e46a608f64684b9dcb417
rob <rstevens>
2022-12-16 23:11:21 +1100

    initial commit

---
+++ lib/slack.py
@@ -0,0 +1,11 @@
+import json, time
+import requests
+from lib.config import *
+
+def getUser(user_id):
+    url = 'https://slack.com/api/users.info'
+    headers = {'Content-type': 'application/x-www-form-urlencoded', 'Authorization': 'Bearer ' + config_slackToken, 'user': user_id}
+    response = requests.get(url, headers=headers)
+    print(response.json())
+    return response.json()
+
