5c56b0dd8a2021cfdd7f5d598082b9b9969e5203
rob <rob>
2023-05-04 21:55:29 +1000

    improve error handling

--- price.py
+++ price.py
@@ -113,19 +113,11 @@ def lambda_handler(chat_id=config_telegramChatID, threshold=config_price_percent
         tickers = [specific_stock]
     else:
         tickers = sharesight.get_holdings_wrapper()
-        if not tickers:
-            # FIXME return a message to user
-            sys.exit(1)
         tickers.update(util.watchlist_load())
     market_data = yahoo.fetch(tickers)
 
     if not specific_stock and days: # else market_data or yahoo.price_history is faster
         performance = sharesight.get_performance_wrapper(days)
-        if not performance:
-            if interactive:
-                # FIXME return a message to user
-                sys.exit(1)
-            sys.exit(1)
         for portfolio_id, data in performance.items():
             for holding in data['report']['holdings']:
                 symbol = holding['instrument']['code']
