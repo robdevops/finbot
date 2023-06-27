6fa27e99b488cdc468b77a84439d3f42ba916271
rob <rob>
2023-06-27 15:00:48 +1000

    experimental Slack image support

--- lib/slack.py
+++ lib/slack.py
@@ -13,10 +13,7 @@ def getUser(user_id):
 def sendPhoto(chat_id, image_data, caption, message_id=None):
     #url = webhooks['telegram'] + "sendPhoto?chat_id=" + str(chat_id)
     url = 'https://slack.com/api/files.upload'
-    #headers = {}
-    #headers = {'Content-type': 'application/json'}
-    headers = {'Content-type': 'multipart/form-data'}
-    headers['Authorization'] = 'Bearer ' + config_slackBotToken
+    headers = {'Authorization': 'Bearer ' + config_slackBotToken}
     data = {
         'channels': chat_id,
         'initial_comment': caption }
