6ec27f804138ca7de977fef00418b32075475b6a
rob <rob>
2023-06-27 15:18:52 +1000

    experimental Slack image support

--- lib/slack.py
+++ lib/slack.py
@@ -31,7 +31,7 @@ def sendPhoto(chat_id, image_data, caption, message_id=None):
     if r.status_code == 200:
         print(r.status_code, "OK Slack sendPhoto", caption)
         output = r.json()
-        if not output['ok']:
+        if output['ok']:
             print("DEBUG")
             print(json.dumps(output, indent=4))
     else:
