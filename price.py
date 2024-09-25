aced6faa46f2b83a84c555492657374b948838a2
rstevens <rstevens@opticomm.net.au>
2024-09-25 19:52:38 +1000

    debug

--- price.py
+++ price.py
@@ -175,9 +175,9 @@ def lambda_handler(chat_id=config_telegramChatID, threshold=config_price_percent
 	if (not specific_stock and days) or (config_performance_use_sharesight and days):
 		performance = sharesight.get_performance_wrapper(days)
 		for portfolio_id, data in performance.items():
-			print("DEBUG", portfolio_id)
+			print("DEBUG", portfolio_id, file=sys.stderr)
 			for holding in data['report']['holdings']:
-				print("DEBUG", holding['instrument']['code'])
+				print("DEBUG", holding['instrument']['code'], file=sys.stderr)
 				symbol = holding['instrument']['code']
 				market = holding['instrument']['market_code']
 				percent = float(holding['capital_gain_percent'])
