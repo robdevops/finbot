0ced6d93f5964c8148edd78e8b2404b390694e6a
rstevens <rstevens@opticomm.net.au>
2025-07-05 10:59:29 +1000

    prevent labels from overlapping title

--- lib/yahoo.py
+++ lib/yahoo.py
@@ -775,15 +775,15 @@ def price_history(ticker, days=None, seconds=config_cache_seconds, graph=config_
 			title_days = days
 			if days > max_days:
 				title_days = max_days
-			title = f"{stock.get('shortName', '')} ({ticker}) {title_days} days {percent_dict[days]}% Current: {stock.get('regularMarketPrice', '')}"
+			title = f"{stock.get('shortName', '')} ({ticker}) {title_days} days {percent_dict[days]}% Last: {stock.get('regularMarketPrice', '')}"
 			caption.append(title)
 		else:
 			for k,v in percent_dict.items():
 				caption.append(str(k) + ": " + str(v) + '%')
 			if 'Max' in percent_dict:
-				title = f"{stock.get('shortName')} ({ticker}) Max {percent_dict.get('Max', '')}% Current: {stock.get('regularMarketPrice', '')}"
+				title = f"{stock.get('shortName')} ({ticker}) Max {percent_dict.get('Max', '')}% Last: {stock.get('regularMarketPrice', '')}"
 			else:
-				title = f"{stock.get('shortName')} ({ticker}) 10Y {percent_dict.get('10Y', '')}% Current: {stock.get('regularMarketPrice', '')}"
+				title = f"{stock.get('shortName')} ({ticker}) 10Y {percent_dict.get('10Y', '')}% Last: {stock.get('regularMarketPrice', '')}"
 		caption = '\n'.join(caption)
 		image_cache_file = "finbot_graph_" + ticker + "_" + str(days) + ".png"
 		image_cache = util.read_binary_cache(image_cache_file, seconds)
