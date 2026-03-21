678ac1ca58332fd2038b9b43596490572d7311f0
rstevens <rstevens@opticomm.net.au>
2026-03-21 15:22:27 +1100

    update super

--- reminder.py
+++ reminder.py
@@ -19,11 +19,9 @@ def lambda_handler():
 		year = str(localtime.strftime('%Y')) # 2023
 		if config_country_code == 'AU':
 			flag = "🇦🇺"
-			if year == "2026" and month_and_day in {'01-28'}:
-				payload = [webhook.bold("🤑 Super payout deadline:", service), webhook.strike("28 Jul 2025", service), webhook.strike("28 Oct 2025", service), "28 Jan 2026 ✅", "28 Apr 2026", "01 Jul 2026 commence payday super"]
-			if year == "2026" and month_and_day in {'04-28'}:
+			if year == "2026" and month_and_day == '04-28':
 				payload = [webhook.bold("🤑 Super payout deadline:", service), webhook.strike("28 Jul 2025", service), webhook.strike("28 Oct 2025", service), webhook.strike("28 Jan 2026", service), "28 Apr 2026 ✅", "01 Jul 2026 commence payday super"]
-			if year == "2026" and month_and_day in {'07-01'}:
+			if year == "2026" and month_and_day == '07-01':
 				payload.append("🤑 Commence payday super ✅")
 			if month_and_day == '06-23':
 				payload.append("💸 Realise capital gains/losses by EOFY June 30" + flag)
