11e981a6b6bc6a2f67ed9e5f435b3e802cd34dd4
rstevens <rstevens@opticomm.net.au>
2026-06-26 01:14:07 +1000

    round to one decimal place so it looks less like currency

--- lib/worker.py
+++ lib/worker.py
@@ -512,7 +512,7 @@ def process_request(service, chat_id, user, message, botName, userRealName, mess
 				webhook.payload_wrapper(service, url, [e], chat_id)
 		for ticker in market_data:
 			try:
-				beta = round(market_data[ticker]['beta'], 2)
+				beta = round(market_data[ticker]['beta'], 1)
 			except KeyError:
 				continue
 			if beta > 1.5 and market_data[ticker]['market_cap'] < 2000000000:
