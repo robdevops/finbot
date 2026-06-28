95e9541378990401f39d3219b2d668bc348a56e0
rstevens <rstevens@opticomm.net.au>
2026-06-28 12:24:09 +1000

    return error on no data

--- lib/reports.py
+++ lib/reports.py
@@ -314,7 +314,7 @@ def prepare_value_payload(service, action='pe', ticker_select=None, length=15):
 				payload.reverse()
 			payload = payload[:length]
 			if not payload:
-				payload = [f"{action}: no data found"])
+				payload = [f"{action}: no data found"]
 			else:
 				payload.insert(0, f"{webhook.bold(f'{heading_type} {length} tracked stocks by {heading_trail} ratio', service)}")
 		return payload
